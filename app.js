ec2857a5d6253d1e99ca948dd069099d313a10ef
NotAnsar <karroauch.ansar@gmail.com>
2023-08-19 01:13:44 +0100

    before deployement2

--- app.js
+++ app.js
@@ -13,47 +13,46 @@ const app = express();
 
 const corsOptions = { origin: process.env.CLIENT_URL, credentials: true };
 
+app.get('/', (req, res) => {
+	res.json('hello');
+});
 app.use(express.json());
 app.use(cors(corsOptions));
 
 // CONNECTING TO DB
-mongoose
-	.connect(
-		`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@clusterelectro.cnlj70e.mongodb.net/?retryWrites=true&w=majority`,
-		{
-			useNewUrlParser: true,
-			useUnifiedTopology: true,
-		}
-	)
-	.then(() => console.log('DB connection successful!'))
-	.catch((err) => console.log(err));
+// mongoose
+// 	.connect(
+// 		`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@clusterelectro.cnlj70e.mongodb.net/?retryWrites=true&w=majority`,
+// 		{
+// 			useNewUrlParser: true,
+// 			useUnifiedTopology: true,
+// 		}
+// 	)
+// 	.then(() => console.log('DB connection successful!'))
+// 	.catch((err) => console.log(err));
 
-app.get('/', (req, res) => {
-	res.json('hello');
-});
+// // ROUTERS
+// app.use('/api/v1/auth', authRouter);
+// app.use('/api/v1/products', productRouter);
+// app.use('/api/v1/order', orderRouter);
+// app.use('/api/v1/users', userRouter);
 
-// ROUTERS
-app.use('/api/v1/auth', authRouter);
-app.use('/api/v1/products', productRouter);
-app.use('/api/v1/order', orderRouter);
-app.use('/api/v1/users', userRouter);
+// app.all('*', (req, res, next) => {
+// 	next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+// });
 
-app.all('*', (req, res, next) => {
-	next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
-});
+// // Register the error-handling middleware after your other routes and middlewares
+// const errorHandler = (err, req, res, next) => {
+// 	const statusCode = err.status || 500;
+// 	const message = err.message || 'Something went wrong.';
 
-// Register the error-handling middleware after your other routes and middlewares
-const errorHandler = (err, req, res, next) => {
-	const statusCode = err.status || 500;
-	const message = err.message || 'Something went wrong.';
-
-	res.status(statusCode).json({
-		status: 'fail',
-		message: message,
-		error: err,
-	});
-};
-app.use(errorHandler);
+// 	res.status(statusCode).json({
+// 		status: 'fail',
+// 		message: message,
+// 		error: err,
+// 	});
+// };
+// app.use(errorHandler);
 
 // RUNNING THE SERVER
 const port = process.env.PORT || 3000;
