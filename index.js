cc90be50b8cca54024589fb824b7f2211de21170
NotAnsar <karroauch.ansar@gmail.com>
2023-08-19 03:17:28 +0100

    before deployement 5

--- index.js
+++ index.js
@@ -5,12 +5,17 @@ const { default: mongoose } = require('mongoose');
 const AppError = require('./utils/appError');
 dotenv.config();
 
-// const corsOptions = {
-// 	origin: process.env.CLIENT_URL,
-// 	credentials: true,
-// 	methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH'],
-// };
-// const corsOptions = { origin: '*' };
+console.log();
+if (!process.env.CLIENT_URL) {
+	app.get('/', (req, res) => {
+		res.json(process.env.CLIENT_URL);
+	});
+}
+const corsOptions = {
+	origin: process.env.CLIENT_URL,
+	credentials: true,
+	methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH'],
+};
 
 const app = express();
 app.get('/', (req, res) => {
@@ -18,19 +23,7 @@ app.get('/', (req, res) => {
 });
 
 app.use(express.json());
-// app.use(cors(corsOptions));
-
-app.use((req, res, next) => {
-	//allow access from every, elminate CORS
-	res.setHeader('Access-Control-Allow-Origin', '*');
-	res.removeHeader('x-powered-by');
-	//set the allowed HTTP methods to be requested
-	res.setHeader('Access-Control-Allow-Methods', 'POST');
-	//headers clients can use in their requests
-	res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-	//allow request to continue and be handled by routes
-	next();
-});
+app.use(cors(corsOptions));
 
 // CONNECTING TO DB
 mongoose
